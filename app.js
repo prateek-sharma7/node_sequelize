65594ad9bb517ab3aa6d7ffd051cd6b260794c55
Prateek <sharmaprateek499@gmail.com>
2023-09-18 18:48:03 +0530

    inserting data and creating product

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
 
 const app = express();
 
@@ -18,7 +20,14 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-//Page not found
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize
+  .sync()
+  .then((result) => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
